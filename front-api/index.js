da36bc0dd98ebe513315803702665ebf68ce4751
Felix WATTEZ <felixwattez@gmail.com>
2019-04-30 17:07:22 +0200

    Implem transactions

--- front-api/index.js
+++ front-api/index.js
@@ -5,15 +5,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = new Router();
 const PORT = 3000;
+const acks = -1;
+const transactionalId = "transactional-id";
 
 const { Kafka, logLevel } = require('kafkajs');
 const kafka = new Kafka({
     clientId: 'producer',
-    brokers: ['kafka:9092']
+    brokers: ['kafka:9092'],
+    maxInFlightRequests: 1
 });
 
 kafka.logger().setLogLevel(logLevel.DEBUG);
-const producer = kafka.producer();
+const producer = kafka.producer({ idempotent: true, transactionalId });
 
 
 app.use('/', router);
@@ -28,14 +31,22 @@ router.get("/ping", (req, res) => {
 
 router.post("/api/message", async (req, res) => {
     const { message, topic } = req.body;
-    await producer.connect();
-    await producer.send({
-        topic: topic,
-        messages: [
-            { value: message },
-        ],
-    })
-    res.send({ message, topic });
+    const transaction = await producer.transaction();
+    try {
+        await transaction.send({
+            acks,
+            topic: topic,
+            messages: [
+                { value: message },
+            ],
+        });
+
+        await transaction.commit();
+        res.send({ message, topic });
+    } catch (e) {
+        await transaction.abort();
+    }
+
 });
 
 (async () => {
